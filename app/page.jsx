08a6faebbb282cd43bef83dea3b86d9814275b67
Jadidi <oussamajadidi2020@gmail.com>
2023-05-15 18:06:40 +0100

    setup the project

---
+++ app/page.jsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <section className="w-full flex-center flex-col">
+      <h1 classname="head_text text-center">
+        Discover & Share
+        <br className="max-md:hidden" />
+        <span className="text-center orange_gradient">AI-Powered Prompts</span>
+      </h1>
+      <p className="desc text-center">Propmptopia is open-source Ai prompting tool for modern world to discover, create and share creative prompts</p>
+    </section>
+  );
+}
